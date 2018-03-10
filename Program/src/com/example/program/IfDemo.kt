e0637d5482df4a38d09229ae9e6202a678e58f66
Santanu Manna <santanu.manna@hotmail.com>
2018-03-10 20:14:07 +0530

    Commit

--- Program/src/com/example/program/IfDemo.kt
+++ Program/src/com/example/program/IfDemo.kt
@@ -6,6 +6,8 @@ fun main(args: Array<String>) {
     val data: Int = 10
     if(data <= 11) {
         println("True data")
+    } else {
+        println("False data")
     }
 
     if(data in 1..10) { //  1<=i and i<=10
